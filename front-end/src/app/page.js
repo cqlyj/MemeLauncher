0f5fa1cf2e60d82488bad2f689af73751875fa9d
cqlyj <luoyingjie0721@gmail.com>
2025-01-08 02:11:40 +0800

    feat: integrate Header component and load blockchain data in Home component

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -1,7 +1,27 @@
+"use client";
+
+import Header from "../components/Header";
+import { useState, useEffect, use } from "react";
+import { ethers } from "ethers";
+
 export default function Home() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+
+  async function loadBlockchainData() {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    setProvider(provider);
+  }
+
+  useEffect(() => {
+    if (window.ethereum) {
+      loadBlockchainData();
+    }
+  }, []);
+
   return (
     <div className="page">
-      <h1 style={{ padding: "1em" }}>Meme Launcher</h1>
+      <Header account={account} setAccount={setAccount} />
     </div>
   );
 }
