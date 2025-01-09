30cb9865d0c3b971083b11a034e359684bf39295
cqlyj <luoyingjie0721@gmail.com>
2025-01-10 02:37:23 +0800

    feat: implement create meme functionality with dynamic fee display

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,36 @@
 "use client";
 
 import Header from "../components/Header";
-import { useState, useEffect, use } from "react";
-import { ethers } from "ethers";
+import List from "../components/List";
+import { useState, useEffect } from "react";
+import { Contract, ethers } from "ethers";
+import { getContractData } from "@/constants";
 
 export default function Home() {
   const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
+  const [launcher, setLauncher] = useState(null);
+  const [fee, setFee] = useState(0);
+  const [showCreate, setShowCreate] = useState(false);
+
+  function toggleCreate() {
+    showCreate ? setShowCreate(false) : setShowCreate(true);
+  }
 
   async function loadBlockchainData() {
     const provider = new ethers.BrowserProvider(window.ethereum);
     setProvider(provider);
+
+    const network = await provider.getNetwork();
+    const chainId = network.chainId;
+
+    const { abi, contractTransaction } = getContractData(chainId);
+    const contractAddress = contractTransaction.transactions[0].contractAddress;
+
+    const launcher = new Contract(contractAddress, abi, provider);
+    setLauncher(launcher);
+    const fee = await launcher.getFee();
+    setFee(fee);
   }
 
   useEffect(() => {
@@ -22,6 +42,29 @@ export default function Home() {
   return (
     <div className="page">
       <Header account={account} setAccount={setAccount} />
+      <main>
+        <div className="create">
+          <button
+            onClick={launcher && account && toggleCreate}
+            className="btn--fancy"
+          >
+            {!launcher
+              ? "[ contract not deployed on this chain ] "
+              : !account
+              ? "[ please connect your wallet ]"
+              : "[ Create A New MEME ]"}
+          </button>
+        </div>
+      </main>
+
+      {showCreate && (
+        <List
+          toggleCreate={toggleCreate}
+          fee={fee}
+          provider={provider}
+          launcher={launcher}
+        />
+      )}
     </div>
   );
 }
