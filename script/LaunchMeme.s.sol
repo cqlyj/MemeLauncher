e1e66fc0eca5c3e9dfa4bdb4b4fe49d0eb05ab9d
cqlyj <luoyingjie0721@gmail.com>
2025-01-13 03:46:20 +0800

    feat: add LaunchMeme script for launching new meme contracts from the Launcher

---
+++ script/LaunchMeme.s.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Launcher} from "src/contracts/Launcher.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {MemeToken} from "src/contracts/MemeToken.sol";
+
+contract LaunchMeme is Script {
+    MemeToken[] public memes;
+
+    function launchMeme(address mostRecentlyDeployment) public {
+        Launcher launcher = Launcher(mostRecentlyDeployment);
+        memes = launcher.getMemes();
+        address launchedMemeAddress = address(memes[0]);
+
+        vm.startBroadcast();
+        launcher.launchMeme(launchedMemeAddress);
+        vm.stopBroadcast();
+
+        console.log("Meme launched!");
+    }
+
+    function run() public {
+        address deployAddress = Vm(address(vm)).getDeployment(
+            "Launcher",
+            uint64(block.chainid)
+        );
+
+        console.log("Most recent Launcher deployment address:");
+        console.logAddress(deployAddress);
+
+        launchMeme(deployAddress);
+    }
+}
